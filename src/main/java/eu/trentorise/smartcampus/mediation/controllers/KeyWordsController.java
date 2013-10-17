f046a9570cd0e4f36665738517bf75360bb5aaba
federicolucca <federico.lucca@gmail.com>
2013-10-17 15:35:15 +0200

    - modify keywords

--- src/main/java/eu/trentorise/smartcampus/mediation/controllers/KeyWordsController.java
+++ src/main/java/eu/trentorise/smartcampus/mediation/controllers/KeyWordsController.java
@@ -22,21 +22,15 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Order;
 import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.query.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import eu.trentorise.smartcampus.mediation.model.CommentBaseEntity;
+
 import eu.trentorise.smartcampus.mediation.model.KeyWord;
-import eu.trentorise.smartcampus.mediation.model.MessageToMediationService;
-import eu.trentorise.smartcampus.network.RemoteConnector;
-import eu.trentorise.smartcampus.network.RemoteException;
 
 @Controller
 public class KeyWordsController {
--- src/main/java/eu/trentorise/smartcampus/mediation/controllers/MediationController.java
+++ src/main/java/eu/trentorise/smartcampus/mediation/controllers/MediationController.java
@@ -24,7 +24,6 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Order;
 import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.query.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -32,10 +31,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import eu.trentorise.smartcampus.mediation.model.CommentBaseEntity;
+
 import eu.trentorise.smartcampus.mediation.model.MessageToMediationService;
-import eu.trentorise.smartcampus.network.RemoteConnector;
-import eu.trentorise.smartcampus.network.RemoteException;
 
 @Controller
 public class MediationController {
